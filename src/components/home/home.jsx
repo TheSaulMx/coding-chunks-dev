64bfa3a112630514f988a020bb7c8f1993cabdc5
TheSaulMx <saaulmx@gmail.com>
2024-02-13 17:45:10 -0700

    home component added with lorem content

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,9 +1,42 @@
+import './style.css'
 
 function Home() {
     return (
-        <>
-            
-        </>
+        <div className='container'>
+            <section className='main-content'>
+                <div className="posts-card">
+                    <h2>Last posts</h2>
+                    <div className='post-card'>
+                        <h3>Post 1</h3>
+
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum pariatur voluptate nisi magni voluptatibus at quos vero laborum debitis doloribus aperiam maiores facere quisquam, sint sapiente perferendis eveniet adipisci natus!</p>
+                    </div>
+                    <div className='post-card'>
+                        <h3>Post 2</h3>
+
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum pariatur voluptate nisi magni voluptatibus at quos vero laborum debitis doloribus aperiam maiores facere quisquam, sint sapiente perferendis eveniet adipisci natus!</p>
+                    </div>
+                    <div className='post-card'>
+                        <h3>Post 3</h3>
+
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum pariatur voluptate nisi magni voluptatibus at quos vero laborum debitis doloribus aperiam maiores facere quisquam, sint sapiente perferendis eveniet adipisci natus!</p>
+                    </div>
+                    <div className='post-card'>
+                        <h3>Post 4</h3>
+
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum pariatur voluptate nisi magni voluptatibus at quos vero laborum debitis doloribus aperiam maiores facere quisquam, sint sapiente perferendis eveniet adipisci natus!</p>
+                    </div>
+                    <div className='post-card'>
+                        <h3>Post 5</h3>
+
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum pariatur voluptate nisi magni voluptatibus at quos vero laborum debitis doloribus aperiam maiores facere quisquam, sint sapiente perferendis eveniet adipisci natus!</p>
+                    </div>
+                </div>
+            </section>
+            <aside className='aside-content'>
+                <p>Aside content</p>
+            </aside>
+        </div>
     );
 }
 
